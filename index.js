87153c04e7d1baf1228cb9b1954c081796c41a8f
unknown <tasnimtanzim55@gmail.com>
2022-05-23 02:27:42 +0600

    check if an user is admin api added

--- index.js
+++ index.js
@@ -151,6 +151,15 @@ async function run() {
             const result = await userCollection.updateOne(filter, updatedUser);
             res.send(result);
         })
+
+        // ONLY ADMIN CAN MAKE ANY USER ADMIN 
+        app.get('/user/:email', verifyJWT, async (req,res) => {
+            const email = req.params.email;
+            const query = {email: email};
+            const user = await userCollection.findOne(query);
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin});
+        })
     }
     finally {
 
